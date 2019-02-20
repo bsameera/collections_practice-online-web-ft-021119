56cbae4cc75476085a68e6a5244a7535f2e3c2af
splendid-frame-7429 <bsameera@yahoo.com>
2019-02-20 06:44:46 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -16,11 +16,11 @@ end
 
 def sort_array_char_count(arr)
   arr.sort do |a, b|
-    if a.length == b
+    if a.length == b.length
       0
-    elsif a < b
+    elsif a.length < b.length
       -1
-    elsif a > b
+    elsif a.length > b.length
       1
     end
   end
